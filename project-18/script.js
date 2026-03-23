26774825c98879b9a17e355c98992c978e00a83a
lakshya <lakshyachandra591@gmai.com>
2026-03-23 17:17:57 +0530

    project18

---
+++ project-18/script.js
@@ -0,0 +1,18 @@
+
+const modal = document.getElementById('myModal');
+const openBtn = document.getElementById('openModalBtn');
+const closeBtn = document.querySelector('.close-btn');
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
